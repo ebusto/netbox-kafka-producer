20ff709785be20413c35dfa5562c9f66f930e84d
Eric Busto <ebusto@nvidia.com>
2019-04-17 14:11:50 -0700

    Simplify pushing to pypi.

---
+++ Makefile
@@ -0,0 +1,8 @@
+default: clean dist
+
+clean:
+	rm -rf build dist netbox_kafka_producer.egg-info
+
+dist:
+	python3 setup.py sdist bdist_wheel
+	python3 -m twine upload dist/*
---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+	long_description = fh.read()
+
+with open('requirements.txt', 'r') as fh:
+	packages = fh.read().splitlines()
+
+setuptools.setup(
+    name='netbox-kafka-producer',
+    version='1.0.1',
+    author='Eric Busto',
+    author_email='ebusto@nvidia.com',
+    description='Easily publish NetBox changes to Kafka',
+	long_description=long_description,
+	long_description_content_type='text/markdown',
+    url='https://github.com/ebusto/netbox-kafka-producer',
+    packages=setuptools.find_packages(),
+	install_requires=packages,
+    classifiers=[
+		'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3',
+    ],
+)
