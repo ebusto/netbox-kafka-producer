ec01db8ad18ceb6003f1089408e1b25f7361b6bc
Eric Busto <ebusto@nvidia.com>
2019-04-23 14:34:01 -0700

    Fix inconsistent spacing.

--- setup.py
+++ setup.py
@@ -7,22 +7,22 @@ with open('requirements.txt', 'r') as fh:
 	packages = fh.read().splitlines()
 
 setuptools.setup(
-    name='netbox-kafka-producer',
-    version='1.0.5',
-    author='Eric Busto',
-    author_email='ebusto@nvidia.com',
-    description='Easily publish NetBox changes to Kafka',
+	name='netbox-kafka-producer',
+	version='1.0.5',
+	author='Eric Busto',
+	author_email='ebusto@nvidia.com',
+	description='Easily publish NetBox changes to Kafka',
 	long_description=long_description,
 	long_description_content_type='text/markdown',
-    url='https://github.com/ebusto/netbox-kafka-producer',
-    packages=setuptools.find_packages(),
+	url='https://github.com/ebusto/netbox-kafka-producer',
+	packages=setuptools.find_packages(),
 	install_requires=packages,
-    classifiers=[
+	classifiers=[
 		'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-    ],
+		'Framework :: Django',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python :: 3',
+	],
 )
