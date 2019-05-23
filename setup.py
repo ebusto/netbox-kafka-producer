3e348d3c535906dd11f72707a4519bd9f0a2975f
Eric Busto <ebusto@nvidia.com>
2019-05-23 12:45:59 -0700

    Version bump.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('requirements.txt', 'r') as fh:
 
 setuptools.setup(
 	name='netbox-kafka-producer',
-	version='1.0.7',
+	version='1.0.8',
 	author='Eric Busto',
 	author_email='ebusto@nvidia.com',
 	description='Easily publish NetBox changes to Kafka',
